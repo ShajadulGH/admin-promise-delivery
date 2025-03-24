4533ca9475762d11d9936390ed02082bac3bceee
Shajadul Karim Bhuiyan <shajadulrahe@gmail.com>
2025-03-24 16:45:16 +0600

    feat-district table

---
+++ app/[lang]/(dashboard)/(loactions)/district/data.js
@@ -0,0 +1,56 @@
+export const data = [
+  {
+    SL: 1,
+    District: "Rajshahi",
+    Division: "Rajshahi",
+    Country: "Bangladesh",
+  },
+  {
+    SL: 2,
+    District: "Barisal",
+    Division: "Barisal",
+    Country: "Bangladesh",
+  },
+  {
+    SL: 3,
+    District: "Dhaka",
+    Division: "Dhaka",
+    Country: "Bangladesh",
+  },
+  {
+    SL: 4,
+    District: "Chattogram",
+    Division: "Chattogram",
+    Country: "Bangladesh",
+  },
+  {
+    SL: 5,
+    District: "Cumilla",
+    Division: "Chittagong",
+    Country: "Bangladesh",
+  },
+  {
+    SL: 6,
+    District: "Sylhet",
+    Division: "Sylhet",
+    Country: "Bangladesh",
+  },
+  {
+    SL: 7,
+    District: "Khulna",
+    Division: "Khulna",
+    Country: "Bangladesh",
+  },
+  {
+    SL: 8,
+    District: "Rangpur",
+    Division: "Rangpur",
+    Country: "Bangladesh",
+  },
+  {
+    SL: 9,
+    District: "Mymensingh",
+    Division: "Mymensingh",
+    Country: "Bangladesh",
+  },
+];
---
+++ app/[lang]/(dashboard)/(loactions)/new-district/layout.jsx
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: "New District",
+};
+
+const Layout = ({ children }) => {
+  return <>{children}</>;
+};
+
+export default Layout;
